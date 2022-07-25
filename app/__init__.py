39ce6ccabe6e00957bd0319f69d70bb93eae45c9
Mamba-Cita <edwnsjohn52@gmail.cm>
2022-07-25 20:56:02 +0300

    db modifications

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ ckeditor = CKEditor(app)
 #app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root@localhost/man"
 
 #online db
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://b3ce1f92dab14b:2b89e72b@us-cdbr-east-06.cleardb.net/heroku_32bfd973b92d25e?reconnect=true"
+app.config['CLEARDB_DATABASE_URL'] = "mysql://b3ce1f92dab14b:2b89e72b@us-cdbr-east-06.cleardb.net/heroku_32bfd973b92d25e?reconnect=true"
 
 
 #initialize db
